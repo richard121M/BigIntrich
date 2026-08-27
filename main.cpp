cae1c3f7a2446e4bbca03bc3d171a1d450923c4c
richard121M <richardtavares520@gmail.com>
2026-08-27 08:15:30 -0300

    bigint v0.001

--- main.cpp
+++ main.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
+class BigInt{
+    public:
+        BigInt(){
+            neg = false;
+            nDig = 1;
+            d = new int(1);
+            d[0] = 0;
+        }
+    private:
+        bool neg;
+        int nDig;
+        int8_t* d = nullptr;
+}
+
 int main(){
+    BigInt n;
+    cin >> n;
     cout << "testando"<< endl;
     return 0;
 }
